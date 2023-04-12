0f1e5c02f7ccb40b5355882f68ac1ea58ab344a6
JesusRueda1 <jdrueda83@misena.edu.co>
2023-04-12 14:32:18 -0500

    borrado todo

--- public/js/mainss.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load',()=>{
-    const container_loader = document.querySelector('.container_loader')
-     container_loader.style.opacity=0
-     container_loader_loader.style.visibility='hidden'
-})
